653879114e326a459f928ab34b99f9326b41ed67
shubhampatwa30 <39874851+shubhampatwa30@users.noreply.github.com>
2020-11-09 22:15:56 -0500

    done with image upload

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const gridFsStorage = require('multer-gridfs-storage');
 const grid = require('gridfs-stream');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const projectModel =  mongoose.model("project");
+
+var username='';
 
 application.use(bodyparser.urlencoded({ 
     extended: true
@@ -34,10 +37,14 @@ application.get("/",(req,res)=>{
     res.render("login_page");
 });
 
-application.post("/",(req,res)=>{
+application.post("/login",(req,res)=>{
   
     projectModel.find(req.body,(err, docs)=>{ 
         if(!err){
+            console.log(docs);
+            let data = docs[0].Username;
+            username = data;
+            console.log(data);
             res.render("list",{data :docs});
            // console.log(docs);
          
@@ -89,7 +96,7 @@ const storage = new gridFsStorage({
           const filename = buf.toString('hex') + path.extname(file.originalname);
           const fileInfo = {
             filename: filename,
-            owner: 'shubham',
+            metadata: {owner: username},
             bucketName: 'uploads'
             
           };
@@ -103,5 +110,69 @@ const storage = new gridFsStorage({
 
   application.post('/upload', upload.single('file'), (req, res) => {
     // res.json({ file: req.file });
-    res.redirect('/');
+    let docs1 = {Username: ''};
+    docs1["Username"] = username;
+    
+    res.render("list",{data :docs1});
+
+  });
+
+  application.get('/displayCollection', (req, res) => {
+
+    gfs.files.find({metadata: {owner : username}}).toArray((err,files) => {
+        let docs1 = {Username: ''};
+if(!files || files.length === 0){
+    
+    docs1["Username"] = username;
+    console.log("Entered if");
+   res.render("list",{data : docs1});
+}
+else{
+    //res.json(files);
+    console.log("Entered else");
+    docs1["Username"] = username;
+files.map(file =>{
+
+    if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+
+
+        file.isImage = true;
+    }else{
+file.isImage = false;
+    }
+
+})
+console.log(files); 
+res.render("coll",{data : docs1, files : files});
+
+
+
+}
+
+    })  
+
+});
+
+
+  
+application.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+  
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+    });
   });
