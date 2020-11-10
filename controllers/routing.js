af9ee4b5f33562e4050b78cc6ce6c5f8ddc7f80b
shubhampatwa30 <39874851+shubhampatwa30@users.noreply.github.com>
2020-11-09 19:28:21 -0500

    uplading done

--- controllers/routing.js
+++ controllers/routing.js
@@ -25,6 +25,4 @@ router.post("/",(req,res)=>{
 
 })
 
-
-
 module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const connection = require("./models");
 const express = require("express");
 const application  = express();
-const path = express("path");
+const path = require("path");
 const expressHandlebars = require("express-handlebars");
 const bodyparser = require("body-parser");
 const routingController = require("./controllers/routing");
-
+const multer =  require('multer');
+const gridFsStorage = require('multer-gridfs-storage');
+const grid = require('gridfs-stream');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 application.use(bodyparser.urlencoded({ 
     extended: true
@@ -26,8 +30,27 @@ application.set("view engine","hbs");
 
 application.get("/",(req,res)=>{
 
-    res.render("index",{});
+    //res.render("index",{});
+    res.render("login_page");
 });
+
+application.post("/",(req,res)=>{
+  
+    projectModel.find(req.body,(err, docs)=>{ 
+        if(!err){
+            res.render("list",{data :docs});
+           // console.log(docs);
+         
+        }
+        else{
+         res.send(err);
+        }
+     }
+    )
+
+
+})
+
 
 application.use("/routing",routingController);
 
@@ -40,3 +63,45 @@ application.listen("3000",(err)=>{
         console.log("Server listening");
     }
 });
+
+
+
+
+const url = 'mongodb://localhost:27017/cloudproject';
+var conn = mongoose.createConnection(url);
+let gfs;
+conn.once('open', ()=> {
+  gfs = grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+  // all set!
+});
+
+
+// Create storage engine
+const storage = new gridFsStorage({
+    url: url,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            owner: 'shubham',
+            bucketName: 'uploads'
+            
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+
+  const upload = multer({ storage });
+
+  application.post('/upload', upload.single('file'), (req, res) => {
+    // res.json({ file: req.file });
+    res.redirect('/');
+  });
